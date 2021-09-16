a4241434f5b0e98c82247a57a2c86a3d7a764123
忑啦米 <907848106@qq.com>
2021-09-16 18:08:23 +0800

    Refactor: use qq mail server

--- sendMail.js
+++ sendMail.js
@@ -2,7 +2,7 @@ const nodemailer = require('nodemailer');
 
 const sendMail = async (data) => {
   let transporter = nodemailer.createTransport({
-    host: 'smtp.163.com',
+    host: 'smtp.qq.com',
     port: '465',
     secureConnection: true,
     auth: {
